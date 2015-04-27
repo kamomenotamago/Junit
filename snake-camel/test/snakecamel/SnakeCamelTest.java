9e7204037b3ce9004ecf70e9160def14d634652f
kagomenotamago <g13929mr@gm.tsuda.ac.jp>
2015-04-28 00:14:01 +0900

    エラー修正

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -7,8 +7,35 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 public class SnakeCamelTest {
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeをcamelにできる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String actual = scu.snakeToCamelcase("rio_asumi_maito_minami");
+		String expected = "RioAsumiMaitoMinami";
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void camelをsnakeにできる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String actual = scu.camelToSnakecase("TotoroMini");
+		String expected = "totoro_mini";
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void 大文字にできる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String actual = scu.capitalize("sakihi");
+		String expected = "Sakihi";
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void 小文字にできる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String actual = scu.uncapitalize("Daimon");
+		String expected = "daimon";
+		assertThat(actual,is(expected));
 	}
 
 }
