656b10b515c60137b313b8d354bbb15a6e3858d4
kagomenotamago <g13929mr@gm.tsuda.ac.jp>
2015-04-28 00:09:29 +0900

    Junitテスト

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -3,40 +3,12 @@ package snakecamel;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.*;
 public class SnakeCamelTest {
-
 	@Test
-	public void snakeをcamelにできる() {
-		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String actual = scu.snakeToCamelcase("rio_asumi_maito_minami");
-		String expected = "RioAsumiMaitoMinami";
-		assertThat(actual,is(expected));
+	public void test() {
+		fail("Not yet implemented");
 	}
-	
-	@Test
-	public void camelをsnakeにできる() {
-		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String actual = scu.camelToSnakecase("TotoroMini");
-		String expected = "totoro_mini";
-		assertThat(actual,is(expected));
-	}
-	
-	@Test
-	public void 大文字にできる() {
-		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String actual = scu.capitalize("sakihi");
-		String expected = "Sakihi";
-		assertThat(actual,is(expected));
-	}
-	
-	@Test
-	public void 小文字にできる() {
-		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String actual = scu.uncapitalize("Daimon");
-		String expected = "daimon";
-		assertThat(actual,is(expected));
-	}
-
 
 }
